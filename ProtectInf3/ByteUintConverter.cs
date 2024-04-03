ee2bd18aaa5c041d1073a4eb7abc141bb4152cd9
EffectStyleS <wasiljewsega@gmail.com>
2024-04-03 22:17:16 +0300

    feat: first

---
+++ ProtectInf3/ByteUintConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace ProtectInf3;
+
+public static class ByteUintConverter
+{
+    public static byte[] ConvertToBytes(IReadOnlyList<uint> data)
+    {
+        var result = new byte[data.Count * 4];
+        for (var i = 0; i < data.Count; i++)
+        {
+            result[i * 4 + 3] = (byte)(data[i] & 0xff);
+            result[i * 4 + 2] = (byte)((data[i] >> 8) & 0xff);
+            result[i * 4 + 1] = (byte)((data[i] >> 16) & 0xff);
+            result[i * 4] = (byte)((data[i] >> 24) & 0xff);
+        }
+
+        return result;
+    }
+
+    public static uint[] ConvertToUint(IReadOnlyList<byte> inputData)
+    {
+        var length = inputData.Count / 4 + ((inputData.Count & 3) == 0 ? 0 : 1);
+        if ((length & 3) != 0)
+        {
+            length += 4 - length % 4;
+        }
+
+        var inputLength = inputData.Count;
+        var result = new uint[length];
+        for (var i = 0; i < result.Length; i ++)
+        {
+            result[i] = i * 4 < inputLength ? inputData[i * 4] : (byte)0;
+            result[i] = (result[i] << 8) | (i * 4 + 1 < inputLength ? inputData[i * 4 + 1] : (byte)0);
+            result[i] = (result[i] << 8) | (i * 4 + 2 < inputLength ? inputData[i * 4 + 2] : (byte)0);
+            result[i] = (result[i] << 8) | (i * 4 + 3 < inputLength ? inputData[i * 4 + 3] : (byte)0);
+        }
+
+        return result;
+    }
+}
---
+++ ProtectInf3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using ProtectInf3;
+
+const string key = "alphabeta9000";
+
+var cryptor = new MarsCryptor();
+
+cryptor.Key = ByteUintConverter.ConvertToUint(SHA384.HashData(Encoding.ASCII.GetBytes(key)));
+
+var dataToEncrypt = File.ReadAllBytes("plain.txt");
+var encrypted = cryptor.Encrypt(ByteUintConverter.ConvertToUint(dataToEncrypt));
+File.WriteAllBytes("encryption.txt", ByteUintConverter.ConvertToBytes(encrypted));
+
+var dataToDecrypt = File.ReadAllBytes("encryption.txt");
+var decrypted = cryptor.Decrypt(ByteUintConverter.ConvertToUint(dataToDecrypt));
+File.WriteAllBytes("decryption.txt", ByteUintConverter.ConvertToBytes(decrypted));
